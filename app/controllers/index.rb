f18db3b6e487bca3e8bd01cb81850ec68b4dd69a
steppinlo <lo.stephaniec@gmail.com>
2015-03-18 18:45:08 -0400

    fixed bugs

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,30 @@
 require 'sinatra'
 
-get '/' do
-  p "Stephanie"
+# get '/' do
+#   "Stephanie"
+# end
+
+#create
+post '/sealions' do
+  redirect '/sealions'
+end
+
+get '/sealions' do
+  'ARK ARK ARK!'
+end
+
+#read
+get '/sealions/:ID' do
+  id = params[:id]
+  "Your ID is #{id}"
+end
+
+#update
+put '/sealions/:ID' do
+  redirect '/sealions'
+end
+
+#delete
+delete '/sealions/:ID' do
+  redirect '/sealions'
 end
