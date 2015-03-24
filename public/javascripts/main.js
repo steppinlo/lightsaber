bbe34c489bc7aafb5b998059b6dc7bd2d152230c
steppinlo <lo.stephaniec@gmail.com>
2015-03-24 19:46:47 -0400

    added click appear/disappear functionality

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,14 +1,28 @@
 var calculator = {
-    add: function(x,y){
-      return (x+y);
-    },
-    multiply: function(x,y){
-      return (x*y);
-    },
-    divide: function(x, y){
-      return (x/y);
-    },
-    subtract: function(y, x){
-      return (y-x);
-    }
+  add: function(x,y){
+    return (x+y);
+  },
+  multiply: function(x,y){
+    return (x*y);
+  },
+  divide: function(x, y){
+    return (x/y);
+  },
+  subtract: function(y, x){
+    return (y-x);
+  }
 };
+
+
+var errorModule = function(){
+  var hidden = document.getElementById("hiddenText");
+  hidden.addEventListener("click", function(){
+    var reveal = document.getElementById("error");
+    if(reveal.classList.contains("hide")){
+      reveal.classList.remove("hide")
+    } else {
+      reveal.classList.add("hide")};
+  })
+};
+
+document.addEventListener("DOMContentLoaded", function(e){errorModule()})
